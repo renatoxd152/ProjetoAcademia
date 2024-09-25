61d64799cb3aa086fa7a3ad35ad9b9a1a4af4231
renatoxd152 <renatomorillo@gmail.com>
2024-09-25 03:38:15 -0300

    feat: Adicionado treino do aluno model

--- academia_projeto/app/Models/Treino_Aluno_Model.php
+++ academia_projeto/app/Models/Treino_Aluno_Model.php
@@ -4,7 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Treino_Aluno_Model extends Model
 {
+    protected $fillable = ['duracao_treino_inicio','duracao_treino_fim','series','aluno_id','treino_id'];
+    protected $table = 'treino_aluno';
+
+    public function aluno():BelongsTo
+    {
+        return $this->belongsTo(Aluno::class,'aluno_id');
+    }
+
+    public function treino():BelongsTo
+    {
+        return $this->belongsTo(Treino_Aluno_Model::class,'treino_id');
+    }
 }
