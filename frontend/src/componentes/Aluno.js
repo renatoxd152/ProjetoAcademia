28ba84ab305891d4a6e7ceb16270acde79e760e4
renatoxd152 <renatomorillo@gmail.com>
2024-10-04 01:44:38 -0300

    feat: Integrações com backend e frontend funcionando

--- frontend/src/componentes/Aluno.js
+++ frontend/src/componentes/Aluno.js
@@ -1,44 +1,148 @@
-import { Nav } from "./Nav"
+import React, { useState } from "react";
+import { Nav } from "./Nav";
 
 export const Aluno = () => {
-    return(
+    const [formData, setFormData] = useState({
+        nome: '',
+        cpf: '',
+        email: '',
+        senha: '',
+        cep: '',
+        telefone: '',
+        dataNascimento: '',
+        tipo: 'aluno'
+    });
+
+    const[mensagem,setMensagem] = useState("");
+    const[erro,setErro] = useState("");
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://127.0.0.1:8000/api/save/user", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    nome: formData.nome,
+                    cpf: formData.cpf,
+                    email: formData.email,
+                    senha: formData.senha,
+                    cep: formData.cep,
+                    telefone: formData.telefone,
+                    data_nasc: formData.dataNascimento,
+                    tipo: formData.tipo
+                }),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                setErro("");
+                setMensagem(data.mensagem)
+            } else {
+                setMensagem("");
+                setErro(data.mensagem || "Erro ao cadastrar o usuário" )
+            }
+        } catch (error) {
+            setMensagem("");
+            setErro("Ocorreu um erro ao cadastrar o usuário.");
+        }
+    };
+
+    return (
         <div className="d-flex">
-            <Nav/> 
+            <Nav />
             <div className="flex-grow-1 p-4">
-                <form> 
+                <form onSubmit={handleSubmit}>
                     <div className="form-group">
+                        <div className="form-group">
+                            <span>{mensagem}</span>
+                            <span>{erro}</span>
+                        </div>
+                        
                         <label>Nome</label>
-                        <input type="text" className="form-control" />
+                        <input
+                            type="text"
+                            name="nome"
+                            className="form-control"
+                            value={formData.nome}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className="form-group">
                         <label>CPF</label>
-                        <input type="text" className="form-control" />
+                        <input
+                            type="text"
+                            name="cpf"
+                            className="form-control"
+                            value={formData.cpf}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className="form-group">
                         <label>Email</label>
-                        <input type="text" className="form-control" />
+                        <input
+                            type="text"
+                            name="email"
+                            className="form-control"
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className="form-group">
                         <label>Senha</label>
-                        <input type="password" className="form-control" />
+                        <input
+                            type="password"
+                            name="senha"
+                            className="form-control"
+                            value={formData.senha}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className="form-group">
                         <label>CEP</label>
-                        <input type="text" className="form-control" />
+                        <input
+                            type="text"
+                            name="cep"
+                            className="form-control"
+                            value={formData.cep}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className="form-group">
                         <label>Telefone</label>
-                        <input type="text" className="form-control" />
+                        <input
+                            type="text"
+                            name="telefone"
+                            className="form-control"
+                            value={formData.telefone}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className="form-group">
                         <label>Data de nascimento</label>
-                        <input type="date" className="form-control" />
+                        <input
+                            type="date"
+                            name="dataNascimento"
+                            className="form-control"
+                            value={formData.dataNascimento}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className="form-group">
-                        <button>Cadastrar Aluno</button>
+                        <button type="submit" className="btn btn-primary">Cadastrar Aluno</button>
                     </div>
                 </form>
             </div>
         </div>
-    )
-}
+    );
+};
--- frontend/src/componentes/Treino.js
+++ frontend/src/componentes/Treino.js
@@ -1,28 +1,82 @@
-import { Nav } from "./Nav"
+import { useState } from "react";
+import { Nav } from "./Nav";
 
 export const Treino = () =>
 {
+    const [formData, setFormData] = useState({
+        nome: '',
+        descricao: '',
+        series: '',
+    });
+    const[mensagem,setMensagem] = useState("");
+    const[erro,setErro] = useState("");
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://127.0.0.1:8000/api/treino", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                setErro("");
+                setMensagem(data.mensagem)
+                setFormData({
+                    nome: '',
+                    descricao: '',
+                    series: '',
+                });
+            } else {
+                setMensagem("");
+                setErro(data.mensagem || "Erro ao cadastrar o treino" )
+            }
+        } catch (error) {
+            setMensagem("");
+            setErro("Ocorreu um erro ao cadastrar o treino.");
+        }
+    };
+
+
+
     return(
         <>
             <div className="d-flex">
                 <Nav/>
 
                 <div className="flex-grow-1 p-4">
-                        <form> 
+                        <form onSubmit={handleSubmit}> 
+
+                            <div className="form-group">
+                                <span>{mensagem}</span>
+                                <span>{erro}</span>
+                            </div>
                             <div className="form-group">
                                 <label>Nome</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name="nome" value={formData.nome} onChange={handleChange} />
                             </div>
 
                             <div className="form-group">
                                 <label>Descrição</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name="descricao" value={formData.descricao} onChange={handleChange}/>
                                 
                             </div>
 
                             <div className="form-group">
                                 <label>Séries</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name="series" value={formData.series} onChange={handleChange} />
                                 
                             </div>
                         
